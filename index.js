5042b7a39f050f41a102624737b1c060ac2938d6
Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>
2022-06-27 21:21:41 +0700

    feat(index): add step 1
    Signed-off-by: Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-import * as fs from 'node:fs';
+import { createReadStream } from 'node:fs';
+import { randomKey, encryptTheKey } from './flow.js';
 import { getFiles } from './util.js';
+
+const key = randomKey();
+
+// step 1: encrypt the key.
+const keyEnc = encryptTheKey(key);
 
 const files = await getFiles();
