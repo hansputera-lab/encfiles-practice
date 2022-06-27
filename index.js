c8baafee2d30244bababa1e2fcab420560ae0cdb
Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>
2022-06-27 21:27:48 +0700

    feat(util): add initFilesOut function
    Signed-off-by: Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 import { createReadStream } from 'node:fs';
 import { randomKey, encryptTheKey } from './flow.js';
-import { getFiles } from './util.js';
+import { getFiles, initFilesOut } from './util.js';
 
+// step 1: prepare variables, and files.
+initFilesOut(); // initialize 'files.out' directory.
 const key = randomKey();
 
-// step 1: encrypt the key.
+// step 2: encrypt the key.
 const keyEnc = encryptTheKey(key);
 
+// step 2.5: read files.
 const files = await getFiles();
+
+for (const file of files) {
+	const readStreamFile = createReadStream(file);
+}
--- util.js
+++ util.js
@@ -1,3 +1,4 @@
+import { existsSync } from 'node:fs';
 import { readdir } from 'node:fs/promises';
 import { resolve } from 'node:path';
 import { cwd } from 'node:process';
@@ -8,3 +9,14 @@ export async function getFiles() {
 		.filter((file) => file.endsWith('.file'))
 		.map((fl) => resolve(cwd(), 'files', fl));
 }
+
+/**
+ * Init 'files.out' directory.
+ * @return {boolean}
+ */
+export const initFilesOut = () => {
+	if (!existsSync(resolve(cwd(), 'files.out'))) {
+		mkdirSync(resolve(cwd(), 'files.out'));
+		return true;
+	} else return false;
+};
