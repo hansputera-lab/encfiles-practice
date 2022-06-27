b518a0ca5f026b0aa4a16b072773b2ad648b23df
Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>
2022-06-27 21:06:11 +0700

    feat: add syme
    Signed-off-by: Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>

---
+++ syme.js
@@ -0,0 +1,33 @@
+/**
+ * Symetric encryption.
+ */
+
+import * as crypto from 'node:crypto';
+
+/**
+ * Create cipher data.
+ * @param {string} key secret key.
+ * @return {{ cipher: crypto.Cipher, iv: Buffer, key: Buffer }}
+ */
+export const createCipher = (key) => {
+	const iv = crypto.randomBytes(16);
+	key = crypto.scryptSync(key, crypto.randomBytes(32), 32);
+
+	const cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
+
+	return {
+		cipher,
+		iv,
+		key,
+	};
+};
+
+/**
+ * Create decipher.
+ * @param {Buffer} iv IV Cipher.
+ * @param {Buffer} key Cipher key.
+ * @return {crypto.Decipher}
+ */
+export const createDecipher = (iv, key) => {
+	return crypto.createDecipheriv('aes-256-cbc', key, iv);
+};
