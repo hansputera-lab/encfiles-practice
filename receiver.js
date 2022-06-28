a52b48fd283de80fe63a32d8447cb51943b94c7b
Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>
2022-06-28 15:51:57 +0700

    feat(receiver): use createInflateRaw instead
    Signed-off-by: Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>

--- receiver.js
+++ receiver.js
@@ -1,4 +1,8 @@
 import { EventEmitter } from 'node:events';
+import { createReadStream } from 'node:fs';
+import { constants as zlibConst, createInflateRaw } from 'node:zlib';
+import { restoreKey } from './flow.js';
+import { createDecipher } from './syme.js';
 
 export const channel = new EventEmitter();
 
@@ -7,5 +11,22 @@ channel.on(
 	/**
 	 * @param {{key: string, iv: string, filePath: string}} param0 File args.
 	 */
-	({ key, iv, filePath }) => {},
+	async ({ key, iv, filePath }) => {
+		// we need to restore the key.
+		key = await restoreKey(Buffer.from(key, 'hex'));
+
+		let buffs = Buffer.alloc(0);
+		const decipher = createDecipher(Buffer.from(iv, 'hex'), key);
+
+		// step 4: decrypt file.
+		const readFileStream = createReadStream(filePath, {
+			'autoClose': true,
+		});
+
+        const decompressor = createInflateRaw({
+            level: zlibConst.Z_BEST_SPEED,
+        });
+
+		readFileStream.pipe(decipher).pipe(decompressor);
+	},
 );
