40b2a9ab84f78962612b7d4433fe868993eadecb
Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>
2022-06-28 16:17:53 +0700

    feat(index): completed flow
    Signed-off-by: Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { randomKey, encryptTheKey } from './flow.js';
 import { createCipher } from './syme.js';
 import { getFiles, initFilesOut } from './util.js';
 import zlib from 'node:zlib';
-import { Transform } from 'node:stream';
+import { channel } from './receiver.js';
 
 // step 1: prepare variables, and files.
 initFilesOut(); // initialize 'files.out' directory.
@@ -15,9 +15,10 @@ const keyEnc = await encryptTheKey(key);
 // step 2.5: read files.
 const files = await getFiles();
 
+// step 3: encrypt.
 for (const file of files) {
 	// symetric encryption..
-    const stream = createReadStream(file, {
+	const stream = createReadStream(file, {
 		'autoClose': true,
 	});
 	const writeFileStream = createWriteStream(
@@ -27,21 +28,22 @@ for (const file of files) {
 		},
 	);
 	const c = createCipher(key);
+	const compressor = zlib.createDeflateRaw({
+		level: zlib.constants.Z_BEST_SPEED,
+		flush: zlib.constants.Z_SYNC_FLUSH,
+	});
 
-	stream.pipe(new Transform({
-        'transform': (chunk, enc, cb) => {
-            chunk = Buffer.from(chunk, enc);
-            chunk = zlib.deflateRawSync(chunk, {
-                level: zlib.constants.Z_BEST_SPEED,
-            });
+	stream.pipe(compressor).pipe(c.cipher).pipe(writeFileStream);
 
-            cb(null, chunk);
-        }
-    })).pipe(c.cipher).pipe(writeFileStream);
+	stream.on('close', () => {
+		console.log(file, 'encrypted and compressed');
+		writeFileStream.close();
 
-	stream.on('finish', () => {
-		writeFileStream.close((err) => {
-			console.log(err);
+		// send to client/receiver to decrypt it.
+		channel.emit('file', {
+			'key': keyEnc.toString('hex'),
+			'iv': c.iv.toString('hex'),
+			'filePath': file.replace('files', 'files.out'),
 		});
 	});
 }
